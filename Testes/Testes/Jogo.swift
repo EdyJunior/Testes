419030181af0b4270c1488ec10048f4daf02e481
Daniel Dias <danieldias@MacBook-Pro-de-Daniel.local>
2016-11-18 11:06:43 -0300

    classe jogo

---
+++ Testes/Testes/Jogo.swift
@@ -0,0 +1,13 @@
+//
+//  Jogo.swift
+//  Testes
+//
+//  Created by Daniel Dias on 18/11/16.
+//  Copyright © 2016 Edvaldo Junior. All rights reserved.
+//
+
+import UIKit
+
+class Jogo: NSObject {
+
+}
